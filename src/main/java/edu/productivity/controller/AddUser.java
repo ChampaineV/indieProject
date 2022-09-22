79ae21abe7556526ce85a52bd53927477a7f0787
Leischow Vang <champainevaj@gmail.com>
2022-09-22 13:53:26 -0500

    Change package name

---
+++ src/main/java/edu/productivity/controller/AddUser.java
@@ -0,0 +1,17 @@
+package edu.productivity.controller;
+
+/*import edu.matc.persistence.UserData;*/
+
+import javax.servlet.annotation.*;
+
+/**
+ * A servlet that'll add the user to the database.
+ * @author lvang
+ */
+
+@WebServlet(
+        urlPatterns = {"/addUser"}
+)
+
+public class AddUser {
+}
