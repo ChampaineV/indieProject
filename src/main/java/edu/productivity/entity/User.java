0fe9ec44be0fca8b5d20de58c307f5e60161b9dd
Leischow Vang <champainevaj@gmail.com>
2022-09-24 14:33:10 -0500

    Add private instance variables to User class

--- src/main/java/edu/productivity/entity/User.java
+++ src/main/java/edu/productivity/entity/User.java
@@ -5,5 +5,11 @@ package edu.productivity.entity;
  * @author lvang
  */
 public class User {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String dateOfBirth;
+
 
 }
