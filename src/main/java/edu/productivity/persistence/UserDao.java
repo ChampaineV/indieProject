6ad86f54e0995179f396afd122b53568aa94760c
Leischow Vang <champainevaj@gmail.com>
2022-09-24 15:03:26 -0500

    Create UserDao and UserDaoTest classes

---
+++ src/main/java/edu/productivity/persistence/UserDao.java
@@ -0,0 +1,20 @@
+package edu.productivity.persistence;
+
+import edu.productivity.entity.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import javax.persistence.criteria.*;
+import java.util.List;
+
+public class UserDao {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    public User insert(User user) {
+        int
+    }
+}
