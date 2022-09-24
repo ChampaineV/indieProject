9c1ba89ea205370914c9e9bb7742dc095d87a5d0
Leischow Vang <champainevaj@gmail.com>
2022-09-24 14:34:34 -0500

    Create getter and setter methods for private instance variables in user

--- src/main/java/edu/productivity/entity/User.java
+++ src/main/java/edu/productivity/entity/User.java
@@ -11,5 +11,43 @@ public class User {
     private String password;
     private String dateOfBirth;
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
