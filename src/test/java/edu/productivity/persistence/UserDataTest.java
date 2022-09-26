f6525e97a07504088e5559443d6558483348d853
Leischow Vang <champainevaj@gmail.com>
2022-09-26 12:31:55 -0500

    Create UserDataTest

---
+++ src/test/java/edu/productivity/persistence/UserDataTest.java
@@ -0,0 +1,18 @@
+package edu.productivity.persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is a test class for UserData.
+ * @author lvang
+ */
+public class UserDataTest {
+}
