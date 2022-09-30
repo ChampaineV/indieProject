563ca18bb64e29e39595045c6f842867682e1f9b
Leischow Vang <champainevaj@gmail.com>
2022-09-30 15:53:20 -0500

    Created Task class

---
+++ src/main/java/edu/productivity/entity/Task.java
@@ -0,0 +1,63 @@
+package edu.productivity.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity(name = "Task")
+@Table(name = "task")
+public class Task {
+
+    private int id;
+    private String task_name;
+    private String description;
+    private User user;
+
+    public Task(){
+    }
+    public Task(String task_name, String description, User user) {
+        this.task_name = task_name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTask_name() {
+        return task_name;
+    }
+
+    public void setTask_name(String task_name) {
+        this.task_name = task_name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", task_name='" + task_name + '\'' +
+                ", description='" + description + '\'' +
+                ", user=" + user +
+                '}';
+    }
+}
