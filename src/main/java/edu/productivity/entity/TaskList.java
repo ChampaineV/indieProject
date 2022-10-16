96a0390852d73da75e7168c7bc3d5b35550e701d
Leischow Vang <champainevaj@gmail.com>
2022-10-16 18:40:51 -0500

    Add OneToMany and ManyToOne annotations to TaskList class

--- src/main/java/edu/productivity/entity/TaskList.java
+++ src/main/java/edu/productivity/entity/TaskList.java
@@ -1,34 +1,60 @@
 package edu.productivity.entity;
 
 
+import org.checkerframework.checker.units.qual.C;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity(name = "TaskList")
 @Table(name = "tasklist")
 
 public class TaskList {
-    private int taskListId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private int id;
+    @Column(name = "list_name")
     private String listName;
+    @Column(name = "number_of_tasks")
     private int numberOfTasks;
-    private int user_id;
-    private int totaltimeSpent;
+    @Column(name = "total_time_spent")
+    /**
+     * Bidirectional @OneToMany
+
+     The bidirectional @OneToMany association also requires a @ManyToOne association on the child side.
+     Although the Domain Model exposes two sides to navigate this association, behind the scenes,
+     the relational database has only one foreign key for this relationship.
+
+     Every bidirectional association must have one owning side only (the child side),
+     the other one being referred to as the inverse (or the mappedBy) side.
+
+     Foreign key is on the child table (Order in this example)
+
+     Source: http://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#associations-one-to-many
+     */
+    @OneToMany(mappedBy = "tasklist", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private Set<Task> tasks = new HashSet<>();
+    @ManyToOne
+    private User user;
 
     public TaskList(){}
 
-    public TaskList(int taskListId, String listName) {
-        this.taskListId = taskListId;
+    public TaskList(int id, String listName) {
+        this.id = id;
         this.listName = listName;
     }
 
-    public int getTaskListId() {
-        return taskListId;
+    public int getId() {
+        return id;
     }
 
-    public void setTaskListId(int taskListId) {
-        this.taskListId = taskListId;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getListName() {
@@ -37,5 +63,68 @@ public class TaskList {
 
     public void setListName(String listName) {
         this.listName = listName;
+    }
+
+    public String calculateTotalTimeSpent(){
+
+        return null;
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTask(Task task){
+        tasks.add(task);
+        task.setUser(this.user);
+    }
+
+    /**
+     * Bidirectional @OneToMany
+
+     The bidirectional @OneToMany association also requires a @ManyToOne association on the child side.
+     Although the Domain Model exposes two sides to navigate this association, behind the scenes,
+     the relational database has only one foreign key for this relationship.
+
+     Every bidirectional association must have one owning side only (the child side),
+     the other one being referred to as the inverse (or the mappedBy) side.
+
+     Foreign key is on the child table (Order in this example)
+
+     By default, the @ManyToOne association assumes that the parent-side entity identifier is to be used to join
+     with the client-side entity Foreign Key column.
+
+     However, when using a non-Primary Key association,
+     the column description and foreign key should be used to instruct Hibernate
+     which column should be used on the parent side to establish the many-to-one database relationship.
+
+     Source: http://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#associations-one-to-many
+     */
+    @ManyToOne
+    @JoinColumn(name = "user_id",
+            foreignKey = @ForeignKey(name = "USER_ID_FK")
+    )
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if ( this == o ) {
+            return true;
+        }
+        if ( o == null || getClass() != o.getClass() ) {
+            return false;
+        }
+        TaskList taskList = (TaskList) o;
+        return Objects.equals( id, taskList.id );
     }
 }
