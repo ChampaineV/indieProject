b1025e734093f630b3408997d0c0a6c7a7a06b79
Leischow Vang <champainevaj@gmail.com>
2022-11-01 15:18:24 -0500

    Create insertTaskListSuccess method in TaskListDataTest class

--- src/test/java/edu/productivity/persistence/TaskListDataTest.java
+++ src/test/java/edu/productivity/persistence/TaskListDataTest.java
@@ -1,24 +1,38 @@
 package edu.productivity.persistence;
 
+import edu.productivity.entity.TaskList;
+import edu.productivity.entity.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class TaskListDataTest {
 
     private final Logger logger = LogManager.getLogger(this.getClass());
-    TaskData taskDao;
+    TaskListData taskListDao;
 
     @BeforeEach
     void setUp() {
-        taskDao = new TaskData();
+        taskListDao = new TaskListData();
         edu.productivity.test.util.Database database = edu.productivity.test.util.Database.getInstance();
         database.runSQL("cleandb.sql");
     }
 
     @Test
-    void insert() {
+    void insertTaskListSuccess() {
+        UserData userDao = new UserData();
+        User user = userDao.getById(2);
+        String taskListName = "Work on Lab 4 - JS";
+        String description = "Lab 4 will demonstrate on how to use JavaScript in a HTML file.";
+        String minutesWorked = "00:59:00";
+        TaskList newTaskList = new TaskList(taskListName, description, LocalTime.parse(minutesWorked), 1, user);
+        int id = taskListDao.insert(newTaskList);
+        assertTrue(taskListDao.getById(id).equals(newTaskList));
     }
 
     @Test
