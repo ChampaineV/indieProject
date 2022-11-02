df6226194139eca9bb6bd2354c24594aa1b297be
Leischow Vang <champainevaj@gmail.com>
2022-11-02 14:41:32 -0500

    Used GenericDao in TaskListDataTest instead of TaskListData

--- src/test/java/edu/productivity/persistence/TaskListDataTest.java
+++ src/test/java/edu/productivity/persistence/TaskListDataTest.java
@@ -16,11 +16,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class TaskListDataTest {
 
     private final Logger logger = LogManager.getLogger(this.getClass());
-    TaskListData taskListDao;
+    GenericDao genericDao;
 
     @BeforeEach
     void setUp() {
-        taskListDao = new TaskListData();
+        genericDao = new GenericDao(TaskList.class);
         edu.productivity.test.util.Database database = edu.productivity.test.util.Database.getInstance();
         database.runSQL("cleandb.sql");
     }
@@ -33,41 +33,41 @@ class TaskListDataTest {
         String description = "Lab 4 will demonstrate on how to use JavaScript in a HTML file.";
         String minutesWorked = "00:59:00";
         TaskList newTaskList = new TaskList(taskListName, description, LocalTime.parse(minutesWorked), 1, user);
-        int id = taskListDao.insert(newTaskList);
-        assertTrue(taskListDao.getById(id).equals(newTaskList));
+        int id = genericDao.insert(newTaskList);
+        assertTrue(genericDao.getById(id).equals(newTaskList));
     }
 
     @Test
     void getByIdSuccess() {
-        TaskList retrievedTaskList = taskListDao.getById(3);
+        TaskList retrievedTaskList = (TaskList) genericDao.getById(3);
         assertNotNull(retrievedTaskList);
-        assertTrue(taskListDao.getById(3).equals(retrievedTaskList));
+        assertTrue(genericDao.getById(3).equals(retrievedTaskList));
     }
 
     @Test
     void saveOrUpdateSuccess() {
         String newListName = "Design Vision/Moodboard";
-        TaskList toUpdateTaskList =  taskListDao.getById(5);
+        TaskList toUpdateTaskList =  (TaskList) genericDao.getById(5);
         toUpdateTaskList.setListName(newListName);
-        taskListDao.saveOrUpdate(toUpdateTaskList);
-        assertTrue(taskListDao.getById(5).equals(toUpdateTaskList));
+        genericDao.saveOrUpdate(toUpdateTaskList);
+        assertTrue(genericDao.getById(5).equals(toUpdateTaskList));
     }
 
     @Test
     void deleteTaskListSuccess() {
-        taskListDao.delete(taskListDao.getById(2));
-        assertNull(taskListDao.getById(2));
+        genericDao.delete(genericDao.getById(2));
+        assertNull(genericDao.getById(2));
     }
 
     @Test
     void getTaskListByPropertyLike() {
-        List<TaskList> taskLists = taskListDao.getTaskListsByPropertyLike("listName", " Class");
+        List<TaskList> taskLists = genericDao.getByPropertyLike("listName", " Class");
         assertEquals(3, taskLists.size());
     }
 
     @Test
     void getAllTaskListsSuccess() {
-        List<TaskList> taskLists = taskListDao.getAllTaskLists();
+        List<TaskList> taskLists = genericDao.getAll();
         assertEquals(5, taskLists.size());
     }
 }
