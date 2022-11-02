79ebe00c484b8f7c87c400b878f78a7a2ffed879
Leischow Vang <champainevaj@gmail.com>
2022-11-02 14:00:29 -0500

    Create constructor method and getSession method for GenericDao class

--- src/main/java/edu/productivity/persistence/GenericDao.java
+++ src/main/java/edu/productivity/persistence/GenericDao.java
@@ -1,4 +1,28 @@
 package edu.productivity.persistence;
 
-public class GenericDao {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+
+public class GenericDao<T> {
+    private Class<T> type;
+    private final Logger logger = LogManager.getLogger(this.class());
+
+    /**
+     * Instantiates Generic DAO
+     * @param type
+     */
+    public GenericDao(Class<T> type) {
+        this.type = type;
+    }
+
+    /**
+     * Return an open session from the SessionFactory
+     * @return
+     */
+    private Session getSession() {
+        return SessionFactoryProvider.getSessionFactory().openSession();
+
+    }
+
 }
