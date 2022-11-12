e4d5a6d848b7cda758f77c24f66f1def5f46c8b7
Leischow Vang <champainevaj@gmail.com>
2022-11-11 21:52:09 -0600

    Working on Cognito files

--- src/main/java/edu/productivity/utilities/PropertiesLoader.java
+++ src/main/java/edu/productivity/utilities/PropertiesLoader.java
@@ -1,20 +1,21 @@
 package edu.productivity.utilities;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.*;
 import java.util.*;
 
 public interface PropertiesLoader {
-
+    Logger logger = LogManager.getLogger(PropertiesLoader.class);
     default Properties loadProperties(String propertiesFilePath) throws Exception {
         Properties properties = new Properties();
         try {
             properties.load(this.getClass().getResourceAsStream(propertiesFilePath));
         } catch (IOException ioException) {
-            ioException.printStackTrace();
-            throw ioException;
+            logger.error("IOException ", ioException);
         } catch (Exception exception) {
-            exception.printStackTrace();
-            throw exception;
+            logger.error("Exception", exception);
         }
         return properties;
     }
