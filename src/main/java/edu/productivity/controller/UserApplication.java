d216b16e5ad57246439be6aa4d2340af4c063968
Leischow Vang <champainevaj@gmail.com>
2022-11-24 19:35:35 -0600

    Create UserApplication class that'll route requests to the service class, UserTasks

---
+++ src/main/java/edu/productivity/controller/UserApplication.java
@@ -0,0 +1,19 @@
+package edu.productivity.controller;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+//Defines the base URI for all resource URIs.
+@ApplicationPath("/service") //You may want to add a value here so that all traffic isn't routed to the class below.
+
+public class UserApplication extends Application {
+    //The method returns a non-empty collection with classes, that must be included in the published JAX-RS application
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet h = new HashSet<Class<?>>();
+        h.add(UserTasks.class );
+        return h;
+    }
+}
